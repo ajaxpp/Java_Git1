9f3e8aa440d7b81ea400ba404f0e1df124088828
Aniwat Ruttanaudom <aniwat.r@tcs.com>
2020-08-13 16:24:07 +0700

    First Commit

---
+++ src/com/code/Hello2.java
@@ -0,0 +1,11 @@
+package com.code;
+
+public class Hello2 {
+	public Hello2() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello2");
+	}
+}
